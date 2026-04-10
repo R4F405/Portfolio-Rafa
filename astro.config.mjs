2bb742936d39244389fa1d21a8148276088f901c
Rafa San Pablo González <sanpablorafa@gmail.com>
2026-04-10 03:23:36 +0200

    udate info and build to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://R4F405.github.io',
+  base: '/Portfolio-Rafa',
+});
